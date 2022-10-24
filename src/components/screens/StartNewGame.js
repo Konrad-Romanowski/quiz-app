7e788d24097245e7c177d7df2d71e07e61bcce7a
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2022-10-24 20:06:22 +0200

    Fixed issue with the UI on smaller desktop screens

--- src/components/screens/StartNewGame.js
+++ src/components/screens/StartNewGame.js
@@ -7,12 +7,10 @@ export default function StartNewGame(props) {
     }
 
     return (
-        <article className='start-new-game'>
-            <section className='start-new-game-menu'>
-                <h2 className='start-new-game-title'>Quiz App</h2>
-                <h3 className='start-new-game-subtitle'>Test your knowledge!</h3>
-                <button className='button start-new-game-button' onClick={startNewGame}>New game</button>
-            </section>
-        </article>
+        <section className='start-new-game'>
+            <h2 className='start-new-game-title'>Quiz App</h2>
+            <h3 className='start-new-game-subtitle'>Test your knowledge!</h3>
+            <button className='button start-new-game-button' onClick={startNewGame}>New game</button>
+        </section>
     )
 }
